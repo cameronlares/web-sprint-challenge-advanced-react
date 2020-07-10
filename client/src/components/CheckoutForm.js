f3492f5da3e2cb062887c9c17ddab2d38c134a2c
Cameron Lares <cameronlares@gmail.com>
2020-07-10 13:54:48 -0400

    Almost MVP

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {useForm} from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -13,18 +14,29 @@ const initialValue = {
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+ function CheckoutForm() {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const [values,handleChanges,handleSubmit,] = useForm(initialValue);
+
+  
+// const CheckoutForm = (props) => {
+//   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+//   const [values, setValues] = useState(initialValue);
+
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setShowSuccessMessage(true);
+  // };
+
+
+
 
   return (
     <>
@@ -69,7 +81,7 @@ const CheckoutForm = (props) => {
         <button>Checkout</button>
       </form>
 
-      {showSuccessMessage && (
+      {useForm.showSuccessMessage && (
         <div className="success-message" data-testid="successMessage">
           <p>
             You have ordered some plants! Woo-hoo! <span role="img">🎉</span>
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,40 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render,screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
 
-test("form shows success message on submit with form details", () => {});
+
+    //render app component
+    render (<CheckoutForm />)
+
+    const header = screen.getByText(/Checkout Form/i)
+    expect(header).toBeInTheDocument()
+
+    //Test will fail if an error is thrown from any function/component
+
+ 
+    // breakTheTest(true)
+    
+});
+
+// function breakTheTest(someValue) {
+
+//     if(someValue===true) {
+//         //do nothing
+//     }else{
+//         throw new Error ("This test failed because we threw an error inside breakTheTest")
+//     }
+
+
+// }
+    test("form shows success message on submit with form details", () => {
+
+        
+
+
+    });
+    
+
